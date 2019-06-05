4c3c4b37659a4e4a777f5180e00b0999c0f51380
Clark Williams <theclarkwilliams@gmail.com>
2019-06-05 17:15:35 -0600

    day3 mvp met, still missing search and like full functionality

--- instagram-app/src/components/Login/LoginPage.js
+++ instagram-app/src/components/Login/LoginPage.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import Login from './Login';
 
-const LoginPage = () => {
+class LoginPage extends React.Component {
+
+    state = {
+        username: "",
+        password: ""
+    }
+
+    handleChanges = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    login = () => {
+        localStorage.setItem("loggedIn", this.state.username);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Login</h1>
+                <form onSubmit={e => this.login (e)}>
+                    <input type="username" name="username" value={this.state.username} required onChange={e => this.handleChanges (e)}/>
+                    <input type="password" name="password" value={this.state.password} required onChange={e => this.handleChanges (e)}/>
+                    <button>Login</button>
+                </form>
+            </div>
+        );
+    }
     
-    return (
-        <div>
-            <h1>Login</h1>
-            <form>
-                <input type="username" />
-                <input type="password" />
-                <button onClick={Login}>Login</button>
-            </form>
-        </div>
-    );
 }
 
 export default LoginPage;
--- instagram-app/src/components/authentication/withAuthenticate.js
+++ instagram-app/src/components/authentication/withAuthenticate.js
@@ -1,30 +1,34 @@
 import React from 'react';
+import PostsPage from '../PostContainer/PostsPage';
+import LoginPage from '../Login/LoginPage';
 
-const withAuthenticate = PostsPageComponent => LoginPage =>
+const withAuthenticate = PostsPage => LoginPage =>
     class extends React.Component {
-        // constructor() {
-        //     super();
-        //     this.state = {
-        //         loggedIn: false;
-        //     }
-        // }
+        constructor() {
+            super();
+            this.state = {
+                loggedIn: false
+            }
+        }
 
-        // componentDidMount() {
-        //     if(){
-        //         this.setState({
-
-        //         })
-        //     } else {
-        //         this.setState({
-                    
-        //         })
-        //     }
-        // }
-
+        componentDidMount() {
+            // check localStorage to see if a use is logged in, and set state accordingly
+            if (localStorage.getItem('loggedIn')) {
+                this.setState({
+                    loggedIn: true
+                })
+            }
+        }    
         render() {
-            return (
-                <PostsPageComponent/>
-            );
+            if (this.state.loggedIn) {
+                return <PostsPage />
+            } else {
+                return <LoginPage />
+            }
+            
+            // return (
+            //     <PostsPageComponent/>
+            // );
         }
     }
 
