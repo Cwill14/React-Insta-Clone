62aa724ab18e48b5197ee73e421a2ef70a94ac60
Clark Williams <theclarkwilliams@gmail.com>
2019-06-05 14:02:39 -0600

    refactor seems complete

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,37 +1,39 @@
 import React from 'react';
-import dummyData from './dummy-data';
-import SearchBar from './components/SearchBar/SearchBar.js';
-import PostContainer from './components/PostContainer/PostContainer.js'
+// import dummyData from './dummy-data';
+// import SearchBar from './components/SearchBar/SearchBar.js';
+// import PostContainer from './components/PostContainer/PostContainer.js'
 import PostsPage from './components/PostContainer/PostsPage';
 import withAuthenticate from './components/authentication/withAuthenticate';
 import './App.scss';
 
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: []
-      // data: dummyData
-    };
-  }
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       data: []
+//       // data: dummyData
+//     };
+//   }
 
-  componentDidMount() {
-    this.setState({
-      data: dummyData
-    })
-  }
+//   // componentDidMount() {
+//   //   this.setState({
+//   //     data: dummyData
+//   //   })
+//   // }
 
-  render() {
-    return (
-      <div>
-        {/* <PostsPage /> */}
-        <SearchBar />
-        <PostContainer postData={this.state.data} />
-      </div>
-    )
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <PostsPage />
+//         {/* <SearchBar />
+//         <PostContainer postData={this.state.data} /> */}
+//       </div>
+//     )
+//   }
+// }
+
+const App = () => <PostsPage />
 
 
 export default App;
--- instagram-app/src/components/PostContainer/PostsPage.js
+++ instagram-app/src/components/PostContainer/PostsPage.js
@@ -1,7 +1,35 @@
 import React from 'react';
+// import dummyData from './dummy-data';
+import dummyData from '../../dummy-data';
+// import SearchBar from './components/SearchBar/SearchBar.js';
+import SearchBar from '../SearchBar/SearchBar.js';
+// import PostContainer from './components/PostContainer/PostContainer.js';
+import PostContainer from '../PostContainer/PostContainer.js';
 
-const PostsPage = () => {
-    
-}
+
+class PostsPage extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        data: []
+        // data: dummyData
+      };
+    }
+  
+    componentDidMount() {
+      this.setState({
+        data: dummyData
+      })
+    }
+  
+    render() {
+      return (
+        <div>
+          <SearchBar />
+          <PostContainer postData={this.state.data} />
+        </div>
+      )
+    }
+  }
 
 export default PostsPage;
